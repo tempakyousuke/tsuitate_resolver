74eef6eebba70aa351000bfb100d99000d0f429a
tempakyousuke <tempakyousuke@gmail.com>
2022-02-26 01:10:30 +0900

    invoke resolve

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,29 @@
   import { SvelteToast } from '@zerodevx/svelte-toast';
   import Position from './board/Position.svelte';
   import { Game } from './game/game';
+  import { invoke } from '@tauri-apps/api/tauri';
 
   const game = new Game();
   game.tsumeInitialize();
+
+  const resolve = async () => {
+    game.cap;
+    game.board;
+    const res = await invoke('resolve', {
+      blackCap: game.cap[1],
+      whiteCap: game.cap[2],
+      board: game.board,
+    });
+    console.log(res);
+  };
 </script>
 
 <main>
   <button>配置設定</button>
-  <Position {game} />
+  <button on:click={resolve}>解く</button>
+  <div class="max-w-lg">
+    <Position {game} />
+  </div>
 </main>
 
 <SvelteToast />
